96b5a90c08535fa156e9e5509ed5be1dcb0ffcdc
abvivek28 <164739858+abvivek28@users.noreply.github.com>
2026-01-22 17:00:04 +0530

    2.5 flash to 2.0 flash

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def summarize_source(title, raw_text, topic):
     """
     
     response = client.models.generate_content(
-        model="gemini-2.5-flash", 
+        model="gemini-2.0-flash", 
         contents=prompt
     )
     return response.text
@@ -303,4 +303,5 @@ The final answer you give should only be the report generated.
         </div>
 
         """, unsafe_allow_html=True)
+
 
